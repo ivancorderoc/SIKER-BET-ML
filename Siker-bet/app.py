b48e6c69e4ef4b2064ab627f9417f671a082179d
ivancordero <122031478+ivancorderoc@users.noreply.github.com>
2024-04-27 11:12:54 +0200

    bug fixes

--- Siker-bet/app.py
+++ Siker-bet/app.py
@@ -3,11 +3,11 @@ import numpy as np
 import pickle
 
 app = Flask(__name__)
-
-# Función para cargar el modelo y el encoder desde los archivos pickle
+root_path = "../Siker-bet/model/"
+# Funcionn para cargar el modelo y el encoder desde los archivos pickle
 def load_model_and_encoder():
-    model = pickle.load(open("model/model.pkl", "rb"))
-    encoder = pickle.load(open("model/encoder.pkl", "rb"))
+    model = pickle.load(open(root_path + "model.pkl", "rb"))
+    encoder = pickle.load(open(root_path + "label_model.pkl", "rb"))
     return model, encoder
 
 @app.route("/")
@@ -27,16 +27,16 @@ def predict():
         if encoded_value in encoder.classes_:
             encoded_value = encoder.transform([encoded_value])[0]
         else:
-            # Si el valor no está presente en el encoder, podemos manejarlo aquí
+            # Si el valor no esta; presente en el encoder, podemos manejarlo aqui;
             # Por ejemplo, podemos asignar un valor por defecto o simplemente ignorarlo
-            encoded_value = 0  # Asignar un valor por defecto de ser necesario
+            encoded_value = 0 # Asignar un valor por defecto de ser necesario
 
         float_features[i] = encoded_value
 
     # Convertir los datos de entrada en un arreglo numpy
     final_features = np.array(float_features, dtype=float)
 
-    # Realizar la predicción utilizando el modelo cargado
+    # Realizar la prediccion utilizando el modelo cargado
     prediction = model.predict(final_features.reshape(1, -1))
 
     if prediction == 1:
@@ -57,4 +57,4 @@ def error():
     return render_template("error.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(port=8000)
